c32c2e3fb3d4795b16ada89024778c6f121036aa
lei <1276753397@qq.com>
2025-12-31 14:40:09 +0800

    feat(PlayerDetailScreen): 添加线性进度指示器组件
    - 导入 LinearProgressIndicator 组件以支持进度显示功能

--- app/src/main/java/com/lei/compose_demo/ui/PlayerDetailScreen.kt
+++ app/src/main/java/com/lei/compose_demo/ui/PlayerDetailScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.activity.compose.BackHandler
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
